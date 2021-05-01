ef5f49cffdd1f63d51f66bb5633c0f1e068d87f3
KIMBEOBWOO <qjqdn1568@naver.com>
2021-05-01 15:33:42 +0900

    cors polcy 적용

--- src/api-test/api-test.controller.ts
+++ src/api-test/api-test.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('api-test')
-export class ApiTestController {}
+export class ApiTestController {
+  @Get()
+  findAll(): string {
+    return 'api-test success';
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ServiceController } from './service/service.controller';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  controllers: [ServiceController, AuthController],
-  providers: [AuthService]
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
