5f2bdff2bf44bdf44c4e2cf49cb477a599963e66
Knoa <mike1124@naver.com>
2021-05-05 22:50:52 +0900

    Implements Social Login Google OAuth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { EmotionCalendarModule } from './emotion-calendar/emotion-calendar.module';
 import { ApiTestModule } from './api-test/api-test.module';
-
+import { ConfigModule} from '@nestjs/config';
 @Module({
-  imports: [AuthModule, EmotionCalendarModule, ApiTestModule],
+  imports: [AuthModule, EmotionCalendarModule, ApiTestModule, ConfigModule.forRoot({
+    envFilePath: '.env.dev',
+    isGlobal: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
