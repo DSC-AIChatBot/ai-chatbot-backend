988a02654342f11acecf3e6db66715cb7bac85b1
knoa0405 <mike1124@naver.com>
2021-05-26 02:20:39 +0900

    merge

---
+++ src/emotion-calendar/emotion-calendar.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { EmotionCalendarService } from './emotion-calendar.service';
+
+@Controller('emotion-calendar')
+export class EmotionCalendarController {
+  constructor(
+    private readonly emotionService: EmotionCalendarService) {}
+
+  @Get('')
+  emotionTest(@Req() req){
+    return this.emotionService.emotionTest()
+  }
+  @Post()
+  emotionCreateTest(@Req() req){
+    return this.emotionService.emotionCreate(req);
+  }
+
+}
