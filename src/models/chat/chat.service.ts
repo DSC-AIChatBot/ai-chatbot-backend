0481d3fbb61aa5ba8be213c67b17dfd23b62772d
KIMBEOBWOO <qjqdn1568@naver.com>
2021-07-01 00:13:30 +0900

    ok

--- src/models/chat/chat.service.ts
+++ src/models/chat/chat.service.ts
@@ -7,7 +7,9 @@ import { postMessagesInput } from './dto/input/post-message.input';
 import { Chat, ChatSchema } from './models/chat.model';
 import { Document } from 'mongoose';
 import axios  from 'axios';
-const DATA_URL = 'http://0.0.0.0:4000/message_post';
+
+const DATA_URL = 'http://192.168.101.182:4000/message_post';
+
 //const TEST_DATA_URL = 'http://0.0.0.0:4000/test';
 @Injectable()
 export class ChatService {
@@ -33,15 +35,10 @@ export class ChatService {
     try {
       const chat = await this.mongoservice.findOne<Chat>({ guestId : userId }, this.chatModel);
       if(chat) {
-        console.log('getget',userId, chat);
         return chat.messages;
       } else {
         return [];
       }
-
-      // console.log('getget', userId, chat);
-
-      // return chat.messages;
     } catch (error) {
       console.log('err',error);
     }
@@ -50,6 +47,9 @@ export class ChatService {
   async postMessage(postMessageData : postMessagesInput) {
     const { userId, role, content } = postMessageData;
 
+    if(content){
+
+   
     console.log('post message data',postMessageData);
     const resContent = await axios({
           url: DATA_URL,
@@ -107,7 +107,7 @@ export class ChatService {
       console.log(error);
     }
     this.pubsub.publish('messageAdded', { messageAdded : { id : this.arrayKey, ...postMessageData } });
-    
+  }
 
     const responsePostMessage = {... postMessageData, role: 'chatbot'};
     const returnData = {
